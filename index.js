a18206f603c6292e8b117bd05be11abcb845a1ed
Nino Ambara Indrawan <ninoambara1@gmail.com>
2023-07-05 15:55:41 +0800

    new

---
+++ index.js
@@ -0,0 +1,14 @@
+function showContent(item) {
+    document.getElementById('content').innerHTML = style
+    };
+
+
+    function showPopup() {
+        var popup = document.getElementById('popup');
+        popup.style.display = 'block';
+      }
+  
+      function hidePopup() {
+        var popup = document.getElementById('popup');
+        popup.style.display = 'none';
+      }
